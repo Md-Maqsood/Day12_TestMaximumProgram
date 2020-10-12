77d0e8f2e156eeec55cc2dae5506a45970dbcfdd
maqsood <maqsoodhamd37@gmail.com>
2020-10-12 14:19:08 +0530

    [Maqsood].refactor: minor refactoring, solved uc3

--- src/main/java/com/bridgeLabs/testMaximum/TestMaximum.java
+++ src/main/java/com/bridgeLabs/testMaximum/TestMaximum.java
@@ -4,45 +4,45 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class TestMaximum {
-	private static Logger logger=LogManager.getLogger(TestMaximum.class);
+	private static Logger logger = LogManager.getLogger(TestMaximum.class);
 
 	public Integer testMaximum(Integer a, Integer b, Integer c) {
-		Integer maxInt=a;
-		if(b.compareTo(maxInt)>0) {
-			maxInt=b;
+		Integer maxInt = a;
+		if (b.compareTo(maxInt) > 0) {
+			maxInt = b;
 		}
-		if(c.compareTo(maxInt)>0) {
-			maxInt=c;
+		if (c.compareTo(maxInt) > 0) {
+			maxInt = c;
 		}
 		return maxInt;
 	}
-	
+
 	public Float testMaximum(Float a, Float b, Float c) {
-		Float maxFloat=a;
-		if(b.compareTo(maxFloat)>0) {
-			maxFloat=b;
+		Float maxFloat = a;
+		if (b.compareTo(maxFloat) > 0) {
+			maxFloat = b;
 		}
-		if(c.compareTo(maxFloat)>0) {
-			maxFloat=c;
+		if (c.compareTo(maxFloat) > 0) {
+			maxFloat = c;
 		}
 		return maxFloat;
 	}
-	
+
 	public String testMaximum(String a, String b, String c) {
-		String maxString=a;
-		if(b.compareTo(maxString)>0) {
-			maxString=b;
+		String maxString = a;
+		if (b.compareTo(maxString) > 0) {
+			maxString = b;
 		}
-		if(c.compareTo(maxString)>0) {
-			maxString=c;
+		if (c.compareTo(maxString) > 0) {
+			maxString = c;
 		}
 		return maxString;
 	}
-	
+
 	public static void main(String[] args) {
-		TestMaximum testMax=new TestMaximum();
-		Integer a=10, b=20, c=15;
-		Integer maxInt=testMax.testMaximum(a, b, c);
-		logger.debug("The maximum integer in "+a+", "+b+", "+c+" is "+maxInt);
+		TestMaximum testMax = new TestMaximum();
+		Integer a = 10, b = 20, c = 15;
+		Integer maxInt = testMax.testMaximum(a, b, c);
+		logger.debug("The maximum integer in " + a + ", " + b + ", " + c + " is " + maxInt);
 	}
 }
