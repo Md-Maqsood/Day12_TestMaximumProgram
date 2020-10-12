2ab3c1dae24d291135daa156827de192316c59b5
maqsood <maqsoodhamd37@gmail.com>
2020-10-12 13:27:30 +0530

    [Maqsood].add: added testMaximum method and test case tc1.1 to check testMaximum method given max integer at first position

--- src/main/java/com/bridgeLabs/testMaximum/TestMaximum.java
+++ src/main/java/com/bridgeLabs/testMaximum/TestMaximum.java
@@ -5,7 +5,18 @@ import org.apache.logging.log4j.Logger;
 
 public class TestMaximum {
 	private static Logger logger=LogManager.getLogger(TestMaximum.class);
-	public static void main(String[] args) {
-		logger.debug("Welcome to test maximum program");
+
+	public Integer testMaximum(Integer a, Integer b, Integer c) {
+		Integer maxInt=0;
+		if(a.compareTo(maxInt)>0) {
+			maxInt=a;
+		}
+		if(b.compareTo(maxInt)>0) {
+			maxInt=b;
+		}
+		if(c.compareTo(maxInt)>0) {
+			maxInt=c;
+		}
+		return maxInt;
 	}
 }
--- src/test/java/com/bridgeLabs/testMaximum/TestMaximumTest.java
+++ src/test/java/com/bridgeLabs/testMaximum/TestMaximumTest.java
@@ -1,20 +1,15 @@
 package com.bridgeLabs.testMaximum;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class TestMaximumTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class TestMaximumTest {
+
+	@Test
+	public void givenMaxIntegerNumberAtPositionOne_ShouldReturnTheSameNumber() {
+		TestMaximum testMax=new TestMaximum();
+		Integer a=20, b=10, c=15;
+		Integer maxInt=testMax.testMaximum(a,b,c);
+		Assert.assertEquals(a,maxInt);
+	}
 }
