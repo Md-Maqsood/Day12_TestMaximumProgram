61445c06990036e5ae6a888061ce63391cc51f43
maqsood <maqsoodhamd37@gmail.com>
2020-10-12 12:48:57 +0530

    [Maqsood].add: initial commit

---
+++ src/main/java/com/bridgeLabs/testMaximum/TestMaximum.java
@@ -0,0 +1,11 @@
+package com.bridgeLabs.testMaximum;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TestMaximum {
+	private static Logger logger=LogManager.getLogger(TestMaximum.class);
+	public static void main(String[] args) {
+		logger.debug("Welcome to test maximum program");
+	}
+}
---
+++ src/test/java/com/bridgeLabs/testMaximum/TestMaximumTest.java
@@ -0,0 +1,20 @@
+package com.bridgeLabs.testMaximum;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestMaximumTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
